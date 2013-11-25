7079d4cd834efbea964feac8c63f4bc4538f61d9
Abhinav Tiwari <abhinavt82@gmail.com>
2013-11-24 18:43:23 -0600

    Inserted Many to Many model association
    Order summary and item menu

--- app/models/item_menu.rb
+++ app/models/item_menu.rb
@@ -1,4 +1,5 @@
 class ItemMenu < ActiveRecord::Base
 	has_many :order_menus
+	has_many :order_summarys, :through => :order_menus
 	mount_uploader :item_picture, PictureUploader 
 end
--- app/models/order_summary.rb
+++ app/models/order_summary.rb
@@ -1,6 +1,6 @@
 class OrderSummary < ActiveRecord::Base
 belongs_to :user
 has_many :order_menus
-
+has_many :item_menus, :through => :order_menus
 
 end
