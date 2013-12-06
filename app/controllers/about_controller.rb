010ffe55e4232ac516bb5e3e5cc7ba6452f04be2
crkundav <ckundavaram2014@kellogg.northwestern.edu>
2013-12-05 19:07:02 -0600

    Update about page
    Added random item

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,11 @@
 class AboutController < ApplicationController
 
 
-  
+  def about
+   	@order_menus = OrderMenu.all
+    @time = Time.new
+    @time = @time.strftime("%A").to_s.downcase
+    @item_menus =  ItemMenu.where(@time=>true)
+  end
 
 end
